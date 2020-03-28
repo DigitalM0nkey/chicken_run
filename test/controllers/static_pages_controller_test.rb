604ce5247b37abe37fc8ce985718d864a0dc9d1b
Terrence Kuiper <github@terrencekuiper.com>
2020-03-27 22:28:54 -0700

    Add static pages and tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = 'Run Chicken Run'
+  end
+
+  test 'should get root' do
+    get static_pages_home_url
+    assert_response :success
+  end
+
   test 'should get home' do
     get static_pages_home_url
     assert_response :success
+    assert_select 'title', "Home | #{@base_title}"
   end
 
   test 'should get help' do
